d25649919351162303b8ad972c2980420cbb211e
ssivenkov <ser.sivenkov@gmail.com>
2024-04-10 14:28:32 +0300

    add users methods "findOne" and "create" to users service, add "signup" method to "Users" controller, add DTO for "Users" controller method "createUser"

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Header, HttpCode, HttpStatus, Post, Body} from '@nestjs/common';
+import {UsersService} from "./users.service";
+import {CreateUserDto} from "./dto/create-user.dto";
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    private readonly userService: UsersService
+  ) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Content-type', 'application/json')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto)
+  }
+}
